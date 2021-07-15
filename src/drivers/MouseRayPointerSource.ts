7b165217337adae98801396a5b2684530a63079b
rafern <rafern@protonmail.com>
2021-07-15 16:11:08 +0100

    Revert "Temporarily add debug prints"
    This reverts commit 8b450c213cf96abefd08e3e8967ffd106ccc2b6e.

--- src/drivers/MouseRayPointerSource.ts
+++ src/drivers/MouseRayPointerSource.ts
@@ -38,9 +38,7 @@ export class MouseRayPointerSource implements RayPointerSource {
 
     private castRay(xNorm: number, yNorm: number, pressing: boolean | null = null) {
         if(this.driver === null)
-            return console.warn(`cast ray cancelled at ${xNorm}, ${yNorm}, pressing ${pressing}`);
-        else
-            console.log(`cast ray at ${xNorm}, ${yNorm}, pressing ${pressing}`);
+            return;
 
         // Get origin and direction. Using a temporary raycaster
         this.raycaster.setFromCamera(
